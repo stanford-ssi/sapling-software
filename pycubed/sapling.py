7abdeac9be64259bd1b74a31adbe8f4d87ab6c2e
Grant Regen <gregen@stanford.edu>
2021-05-20 20:50:49 -0700

    starting folder for pycubed code

---
+++ pycubed/sapling.py
@@ -0,0 +1,14 @@
+"""
+The Sapling class is the highest level class for interfacing with PyCubed and
+other payload hardware. 
+
+Attributes:
+    status (bool):
+
+Methods:
+    XXX: Summary
+
+"""
+class Sapling:
+    def __init__(self):
+        self.__status = False
