abb52931d5218526ae95815fd057d78807bd8451
Flynn Dreilinger <swashbuckling.cosmoknot@gmail.com>
2022-01-08 21:56:25 +0000

    smol

--- coral/setup.sh
+++ coral/setup.sh
@@ -1,8 +1,11 @@
 # set up virtual environment
 sudo apt-get -y install python3-venv
-sudo python3 -m venv venv
-source venv/bin/activate
-pip install requirements.txt
+sudo python3 -m venv $HOME/venv
+source $HOME/venv/bin/activate
+sudo pip install --user -r requirements.txt
+
+# move code to the root folder
+mv ./src/* $HOME/
 
 # install service
 sudo cp /home/mendel/sapling-software/coral/services/command_handler.service /lib/systemd/system/sapling_coral_command_handler.service
