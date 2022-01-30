6bd69e54fef8de4dc4041026b53c458bf705bbbd
Flynn Dreilinger <swashbuckling.cosmoknot@gmail.com>
2022-01-30 22:04:11 +0000

    systemd process now runs on startup

--- coral/reset.sh
+++ coral/reset.sh
@@ -1,5 +1,6 @@
 sudo systemctl stop sapling_coral_command_handler
-sudo cp /home/mendel/sapling-software/coral/services/command_handler.service /lib/systemd/system/sapling_coral_command_handler.service
+sudo cp /home/mendel/sapling-software/coral/services/command_handler.service /etc/systemd/system/sapling_coral_command_handler.service
 sudo systemctl daemon-reload
+sudo systemctl enable sapling_coral_command_handler
 sudo systemctl start sapling_coral_command_handler
 
