76d68af378260ebc8c2646a78829fa446d6d0430
Adrian Gamarra <adriangamarra2018@gmail.com>
2022-04-28 05:04:37 +0000

    Added some logic and glare image

--- coral/src/earth.py
+++ coral/src/earth.py
@@ -5,9 +5,11 @@ import numpy as np
 image = Image.open('img0000.jpg')
 imageR = Image.open('img0001.jpg')
 sunImage = Image.open("sunEarthPic.jpeg")
+glareImage = Image.open('img0003.jpg')
 
 def analyzeImage(image):
     np_img = np.array(image)
+    print(image.filename)
     print(image.size)
     print(np_img.shape)
 
@@ -15,7 +17,7 @@ def analyzeImage(image):
     print(f"Total Pixels: {totalPixels}")
 
     #Test for actual expected thresholds
-    black = np.count_nonzero(np.all(np_img < [30, 30, 30], axis = 2));
+    black = np.count_nonzero(np.all(np_img < [45, 45, 45], axis = 2));
     white = np.count_nonzero(np.all(np_img > [235, 235, 235], axis = 2));
     
     print(f"Black pixels: {black}")
@@ -26,9 +28,22 @@ def analyzeImage(image):
 
     print(f"Percentage black pixles: {blackPercent}")
     print(f"Percentage white pixels: {whitePercent}")
+
+    unusablePercent = round(blackPercent + whitePercent, 3)
+    print(f"Combined unusable percent: {unusablePercent}")
+
+    if unusablePercent > 0.6:
+        print("Don't send image down\n")
+        return False
+    else:
+        print("Send image down\n")
+        return True
+
+    # by default send image down
     print()
+    return true
 
 analyzeImage(image)
 analyzeImage(imageR)
 analyzeImage(sunImage)
-
+analyzeImage(glareImage)
